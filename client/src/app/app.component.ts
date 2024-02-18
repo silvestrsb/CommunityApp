6219911778aa2ef795e537ae4e80b35e63eccfc9
Silvestrs <silvestrs.b@gmail.com>
2024-02-18 17:06:57 +0200

    Angulat 16 setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Community App';
-  users: any;
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
-  }
+export class AppComponent {
+  title = 'client';
 }
