2682773898a1554d1879ea8a337c013834774a13
Silvestrs <silvestrs.b@gmail.com>
2024-04-18 18:51:26 +0300

    Add EF Relationships, Seeding Database, Repository Pattern, Automapper

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -7,14 +7,15 @@ public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services,
          IConfiguration config)
-         {
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors();
-            services.AddScoped<ITokenService, TokenService>();
-
-            return services;
-         }
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        return services;
+    }
 }
