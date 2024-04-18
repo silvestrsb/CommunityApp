d776ea9f39695c4a715a4c029421a7110575559c
Silvestrs <silvestrs.b@gmail.com>
2024-04-18 12:30:49 +0300

    Add Exception Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,4 @@
 using API;
-using API.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +12,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader()
     .AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
