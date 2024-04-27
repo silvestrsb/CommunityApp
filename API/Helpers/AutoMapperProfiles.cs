8c415256f430b55171bdf3812a9dae857f0632f1
Silvestrs <silvestrs.b@gmail.com>
2024-04-27 15:00:48 +0300

    Add Angular Register Form

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -12,5 +12,6 @@ public class AutoMapperProfiles : Profile
             .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
         CreateMap<Photo, PhotoDto>();
         CreateMap<MemberUpdateDto, AppUser>();
+        CreateMap<RegisterDto, AppUser>();
     }
 }
