cf120b191d8f8d23e7e83e13e71daea0e0638baf
Silvestrs <silvestrs.b@gmail.com>
2024-05-02 23:02:26 +0300

    Like feature added

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+
+namespace API;
+
+public interface ILikesRepository
+{
+    Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+    Task<AppUser> GetUserWithLikes(int userId);
+    Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesPrams);
+}
