f180d89491d19042037221fb677bd06d15fdcb96
Silvestrs <silvestrs.b@gmail.com>
2024-05-21 16:16:30 +0300

    Add UnitOfWork Pattern, confirm dialog service, optimizations EF queries

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -15,13 +15,11 @@ public static class ApplicationServiceExtensions
         });
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
-        services.AddScoped<IUserRepository, UserRepository>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         services.AddScoped<IPhotoService, PhotoService>();
         services.AddScoped<LogUserActivity>();
-        services.AddScoped<ILikesRepository, LikesRepository>();
-        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddSignalR();
         services.AddSingleton<PresenceTracker>();
 
---
+++ client/src/app/_services/confirm.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { initialState } from 'ngx-bootstrap/timepicker/reducer/timepicker.reducer';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmService implements OnInit {
+  bsModalRef?: BsModalRef<ConfirmDialogComponent>;
+
+  constructor(private modalService: BsModalService) { }
+  ngOnInit(): void {
+  }
+
+  confirm(
+    title = 'Confirmation',
+    message = 'Are you sure you want to do this?',
+    btnOkText = 'Ok',
+    btnCancelText = 'Cancel'
+  ): Observable<boolean> {
+    const config = {
+      initialState: {
+        title,
+        message,
+        btnOkText,
+        btnCancelText
+      }
+    }
+    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
+    return this.bsModalRef.onHidden!.pipe(
+      map(() => {
+        return this.bsModalRef!.content!.result
+      })
+    )
+  }
+}
