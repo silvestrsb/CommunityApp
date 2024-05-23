089dcb558b0ee944c4cee2583a69481fb6b56ceb
Silvestrs <silvestrs.b@gmail.com>
2024-05-23 21:18:30 +0300

    Switch from SqlLite to Postgres, add Dockerfile, publishing changes

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -10,6 +10,11 @@ namespace API;
 
 public class Seed
 {
+    public static async Task ClearConnections(DataContext context)
+    {
+        context.Connections.RemoveRange(context.Connections);
+        await context.SaveChangesAsync();
+    }
     public static async Task SeedUsers(UserManager<AppUser> userManager,
         RoleManager<AppRole> roleManager)
     {
@@ -36,6 +41,8 @@ public class Seed
         foreach (var user in users)
         {
             user.UserName = user.UserName.ToLower();
+            user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+            user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);
             await userManager.CreateAsync(user, "Pa$$w0rd");
             await userManager.AddToRoleAsync(user, "Member");
             user.Photos.SingleOrDefault().IsApproved = true;
--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -3,3 +3,5 @@ export const environment = {
     apiUrl: 'https://localhost:5001/api/',
     hubUrl: 'https://localhost:5001/hubs/'
 };
+
+
